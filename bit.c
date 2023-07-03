edd51bede2799ebd936f7a05e1d45de828183cf1
jaya_09 <83871019+jaya-09@users.noreply.github.com>
2023-07-03 21:17:07 +0530

    Update bit.c

--- bit.c
+++ bit.c
@@ -1,41 +1,94 @@
-/*Bit stuffing*/
+#include <stdio.h>
 
-#include<stdio.h>
-#include<string.h>
+void bitStuffing(int arr[], int n)
+{
+    int count1 = 0;// to count no of 1's
+    int k = 0;//new array index
+    int array[20];//new output(stuffed) array
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            count1++;
+            array[k] = arr[i];
+            k++;
+            //if a zero is followed by 5 1's then append 0 -->bit stuffing
+            if (count1 == 5 && arr[i - 5] == 0)
+            {
+                array[k] = 0;
+                k++;
+            }
+        }
+        else
+        {
+            count1 = 0;
+            array[k] = arr[i];
+            k++;
+        }
+    }
+
+    printf("Bit-stuffed array: ");
+    for (int a = 0; a < k; a++)
+    {
+        printf("%d ", array[a]);
+    }
+    printf("\n");
+}
+
+void bitUnstuffing(int arr[], int n)
+{
+    int count1 = 0;
+    int k = 0;
+    int array[20];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            count1++;
+            array[k] = arr[i];
+            k++;
+            //if a zero is followed by 5 1's, skip the appended 0 -->bit unstuffing
+            if (count1 == 5 && arr[i-5]==0 && arr[i + 1] == 0)
+            {
+                i++;
+            }
+        }
+        else
+        {
+            count1 = 0;
+            array[k] = arr[i];
+            k++;
+        }
+    }
+
+    printf("Bit-unstuffed array: ");
+    for (int a = 0; a < k; a++)
+    {
+        printf("%d ", array[a]);
+    }
+    printf("\n");
+}
+
 int main()
 {
-    char ch,arr[100],flag[100]=" ",temp[100]=" ";
-    int counter=0,j=0,i=0;
-    printf("enter the bit string:");
-    scanf("%s",arr);
-    strcat(flag,"-01111110-");
-    
-    if(strlen(arr)<5)
+    int n;
+
+    printf("Enter the size of the bit string to be sent: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the bit string of length %d: ", n);
+    for (int i = 0; i < n; i++)
     {
-        strcat(arr,flag);
-        strcat(flag,arr);
-        printf("the bits after stuffing  %s",flag);
+        scanf("%d", &arr[i]);
     }
-    
-    
-    else
-    {
-        while((ch=arr[j])!='\0')
-        {
-            if(ch=='1') ++counter;
-            else counter=0;
-            temp[i++]=ch;
-            if(counter==5)
-            {
-                temp[i++]='0';
-                counter=0;
-            }
-            j++;
-        }
-        temp[i]='\0';
-        strcat(temp,flag);
-        strcat(flag,temp);
-        printf("The bits after stuffing %s",flag);
-    }
+
+    bitStuffing(arr, n);//encoding
+    printf("----------------------------------------------------------------\n");
+    bitUnstuffing(arr, n);//decoding
+
     return 0;
 }
